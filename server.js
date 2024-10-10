a63d6bf9fc3fc0c5efadecfba5ec702961c792a4
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2024-10-10 14:43:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/', publicRoutes)
-app.use('/', auth, privateRoutes)
+app.use('/', privateRoutes)
 
 app.listen(3000, () => console.log("Servidor Rodando"))
